c39ee4eacae7413cb861413542eed35f20df149d
AysenAlpsln <aysenalpaslan01@gmail.com>
2022-12-20 21:32:24 +0300

    edit icons and add icons folder

--- src/components/Days.js
+++ src/components/Days.js
@@ -35,6 +35,30 @@ const Days = () => {
     }
   }
 
+  const getIcon = (icon) => {
+    console.log(typeof icon, icon)
+    switch(icon) {
+      case "03n":
+        return "icons/cloud.png";
+        break;
+        
+      case "01n":
+        return "icons/sun.png";
+        break;
+
+      case "10n":
+        return "icons/rainy.png";
+        break;
+
+      case "13n":
+        return "icons/sun.png";
+        break;
+
+      default:
+        return ""
+    }
+  }
+
   return (
     <div className="weather-container">
       <div className="today-weather-details-container">
@@ -45,13 +69,13 @@ const Days = () => {
           <p className="wind">{temps[0].windSpeed} km/h Wind</p>
         </div>
         {/* <img className="today-weather-status-image" src="https://assets.ccbp.in/frontend/intermediate-rwd/partly-sunny-img.png" /> */}
-        <img className="today-weather-status-image" src={`https://openweathermap.org/img/wn/${icons[0]}@4x.png`} />
+        <img className="today-weather-status-image" src={getIcon(icons[0])} />
       </div>
       <div className="weekly-weather-details-container">
         
         <div className="daily-weather-details-card">
           <p className="date">Today</p>
-          <img className="weather-status-image" src={`https://openweathermap.org/img/wn/${icons[0]}@2x.png`} />
+          <img className="weather-status-image" src={getIcon(icons[0])} />
           <div className="temperature-range-container">
             <p className="max-temperature">{temps[0].max}<sup>o</sup></p>
             <p className="min-temperature">{temps[0].min}<sup>o</sup></p>
@@ -61,7 +85,7 @@ const Days = () => {
         
         <div className="daily-weather-details-card">
           <p className="date">{days[1]}</p>
-          <img className="weather-status-image" src={`https://openweathermap.org/img/wn/${icons[1]}@2x.png`} />
+          <img className="weather-status-image" src={getIcon(icons[1])} />
           <div className="temperature-range-container">
             <p className="max-temperature">{temps[1].max}<sup>o</sup></p>
             <p className="min-temperature">{temps[1].min}<sup>o</sup></p>
@@ -71,7 +95,7 @@ const Days = () => {
 
         <div className="daily-weather-details-card">
           <p className="date">{days[2]}</p>
-          <img className="weather-status-image" src={`https://openweathermap.org/img/wn/${icons[2]}@2x.png`} />
+          <img className="weather-status-image" src={getIcon(icons[2])} />
           <div className="temperature-range-container">
             <p className="max-temperature">{temps[2].max}<sup>o</sup></p>
             <p className="min-temperature">{temps[2].min}<sup>o</sup></p>
@@ -81,7 +105,7 @@ const Days = () => {
 
         <div className="daily-weather-details-card">
           <p className="date">{days[3]}</p>
-          <img className="weather-status-image" src={`https://openweathermap.org/img/wn/${icons[3]}@2x.png`} />
+          <img className="weather-status-image" src={getIcon(icons[3])} />
           <div className="temperature-range-container">
             <p className="max-temperature">{temps[3].max}<sup>o</sup></p>
             <p className="min-temperature">{temps[3].min}<sup>o</sup></p>
@@ -91,7 +115,7 @@ const Days = () => {
 
         <div className="daily-weather-details-card">
           <p className="date">{days[4]}</p>
-          <img className="weather-status-image" src={`https://openweathermap.org/img/wn/${icons[4]}@2x.png`} />
+          <img className="weather-status-image" src={getIcon(icons[4])} />
           <div className="temperature-range-container">
             <p className="max-temperature">{temps[4].max}<sup>o</sup></p>
             <p className="min-temperature">{temps[4].min}<sup>o</sup></p>
